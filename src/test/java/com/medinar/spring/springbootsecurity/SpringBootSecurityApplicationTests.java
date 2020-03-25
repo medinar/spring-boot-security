200815565f5b7ca316105bb686ea5dc4a7f14f74
rommelmedina <rommelmedina@192.168.100.4>
2020-03-25 14:23:04 +0800

    Initial commit for Spring Boot Security

---
+++ src/test/java/com/medinar/spring/springbootsecurity/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medinar.spring.springbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
